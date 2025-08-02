fcba9bb4d3f6ea0d64ebb926f510376e555097de
Ritesh Kumar <riteshkumar.nitk21@gmail.com>
2025-08-02 09:09:18 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/invoice-preview.tsx
+++ src/components/invoice-preview.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import * as React from "react";
 import type { Invoice } from "@/lib/schemas";
 import { format } from "date-fns";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
